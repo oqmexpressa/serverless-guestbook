0b5f3bd425648e923b509d4df79f0dd7467ffa79
oqmexpressa <albertabone@gmail.com>
2022-08-01 17:13:45 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f3849737.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
